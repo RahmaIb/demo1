f1766059f4adc576907f548ba1bdfac93b43d034
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2025-10-30 19:36:40 -0500

    Change HelloApplication.java

--- src/main/java/edu/utsa/cs3443/demo1/Launcher.java
+++ src/main/java/edu/utsa/cs3443/demo1/Launcher.java
@@ -3,7 +3,5 @@ package edu.utsa.cs3443.demo1;
 import javafx.application.Application;
 
 public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
+
 }
