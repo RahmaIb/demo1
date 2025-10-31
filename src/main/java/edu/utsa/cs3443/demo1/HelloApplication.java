7539879104429475b02b4aff7251d639e7d54173
shobha <shobhaoad@gmail.com>
2025-10-31 14:08:50 -0500

    Updated the window title to "Welcome to ChirpCheck Group 9!" for Git workflow test

--- src/main/java/edu/utsa/cs3443/demo1/HelloApplication.java
+++ src/main/java/edu/utsa/cs3443/demo1/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Welcome to ChirpCheck Group 9!");
         stage.setScene(scene);
         stage.show();
     }
